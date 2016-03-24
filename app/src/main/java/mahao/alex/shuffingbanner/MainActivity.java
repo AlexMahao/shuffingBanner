66e51ed3eff9e2a18ca09663180ba44e671054e3
Alex_Mahao <zziamahao@163.com>
2016-03-24 16:06:59 +0800

    one

---
+++ app/src/main/java/mahao/alex/shuffingbanner/MainActivity.java
@@ -0,0 +1,39 @@
+package mahao.alex.shuffingbanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    private ShufBanner mShufBanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShufBanner = ((ShufBanner) findViewById(R.id.shuf));
+
+        List<String> mImages = new ArrayList<>();
+        mImages.add("http://pic.huodongjia.com/event/2015-11-18/event151612.jpg");
+        mImages.add("http://pic.huodongjia.com/event/2016-03-17/event177131.jpg");
+        mImages.add("http://pic.huodongjia.com/event/2015-12-03/event156106.jpg");
+        mShufBanner.startShuf(mImages,true);
+        mShufBanner.setItemClcikListener(new ShufBannerClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void refresh(View view){
+
+    }
+}
---
+++ app/src/main/java/mahao/alex/shuffingbanner/ShufBannerClickListener.java
@@ -0,0 +1,12 @@
+package mahao.alex.shuffingbanner;
+
+/**
+ * Created by mdw on 2016/3/24.
+ */
+public interface ShufBannerClickListener {
+    /**
+     *
+     * @param position -1 未设置任何点击
+     */
+    void onClick(int position);
+}
